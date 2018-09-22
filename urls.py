aa55b9174e3f14ac4801d07457a1c6565c6e0b7e
Thuwarakesh <thuwarakesh@gmail.com>
2018-09-22 20:39:43 +0530

    views and route created

--- urls.py
+++ urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+    path('adventures', views.adventures, name='adventures'),
+    path('adventures/<str:qualifier>/',
+         views.adventure, name='adventure'),
+    path('events', views.events, name='events'),
+    path('events/<str:qualifier>/',
+         views.event, name='event'),
+    path('gallery', views.gallery, name='gallery'),
+    path('album/<str:qualifier>/', views.album, name='album'),
 ]
--- views.py
+++ views.py
@@ -1,6 +1,47 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.views.generic import ListView, DetailView
+from .models import Adventure, Event, Album
 # Create your views here.
+
+
 def home(request):
-    return HttpResponse('Hai')
+    return render(request, 'explorers/home.html', {})
+
+
+def about(request):
+    return render(request, 'explorers/about.html', {})
+
+
+def contact(request):
+    return render(request, 'explorers/contact.html', {})
+
+
+def adventures(request):
+    advs = Adventure.objects.filter(is_published=True)
+    return render(request, 'explorers/adventure_list.html', {'adventures': advs})
+
+
+def adventure(request, qualifier):
+    adv = Adventure.objects.get(page_qualifier=qualifier)
+    return render(request, 'explorers/adventure_detail.html', {'adventure': adv})
+
+
+def events(request):
+    evts = Event.objects.filter(is_published=True)
+    return render(request, 'explorers/event_list.html', {'events': evts})
+
+
+def event(request, qualifier):
+    evt = Event.objects.get(page_qualifier=qualifier)
+    return render(request, 'explorers/event_detail.html', {'event': evt})
+
+
+def gallery(request):
+    albums = Album.objects.filter(is_published=True)
+    return render(request, 'explorers/gallery.html', {'albums': albums})
+
+
+def album(request, qualifier):
+    album = Album.objects.get(page_qualifier=qualifier)
+    return render(request, 'explorers/album.html', {'album': album})
