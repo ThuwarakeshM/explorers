6d2e8221c66d6125882020c5cbc71756270be465
Thuwarakesh <thuwarakesh@gmail.com>
2018-09-24 20:51:19 +0530

    form classes for contact forms

--- forms.py
+++ forms.py
@@ -1,7 +1,22 @@
 from django import forms
 
+
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    phone = forms.CharField(max_length=20)
-    message = forms.CharField(widget=forms.Textarea)
-    email = forms.EmailField()
+    name = forms.CharField(
+        label='Name',
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control'
+        })
+    )
+    phone = forms.CharField(
+        max_length=20,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control'
+        }))
+    message = forms.CharField(widget=forms.Textarea(attrs={
+        'class':'form-control',
+    }))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        'class': 'form-control',
+    }))
